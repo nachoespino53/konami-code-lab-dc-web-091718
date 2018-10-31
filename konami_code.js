ac23344f39fdbebdc9222931056eb801dbc732ef
nachoespino53 <nachoespino53@gmail.com>
2018-10-30 21:19:58 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
+
+
+
 function init() {
-  // your code here
+  let index = 0;
+  document.body.addEventListener('keydown', (e) => {
+    let key = e.key;
+    if (codes[index] === key) {
+      if (index === codes.length - 1){
+        window.alert("Hurray!");
+        idx = 0
+      } else {
+        index++;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
